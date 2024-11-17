8e85e4394dc4f85384d9f2a94d4d2d7e94880153
joshoti <joshoti539@gmail.com>
2024-11-16 19:14:05 -0800

    Added hugging face text gen trasnformer pipeline

---
+++ notebooks/hugging_face_text_gen.py
@@ -0,0 +1,18 @@
+from typing import Dict, List
+
+from transformers import pipeline
+
+from interfaces import ITextGenTransformerHelper
+
+
+class HuggingFaceTextGenerationClient(ITextGenTransformerHelper):
+    model_name = "HuggingFaceTB/SmolLM2-1.7B-Instruct"
+
+    def generate_human_readable_text(self, messages: List[Dict[str]]):
+        """Check super class for sample message format"""
+
+        pipe = pipeline("text-generation", model=self.model_name)
+        return pipe(messages, max_new_tokens=128)
+
+
+hugging_face_text_gen_client = HuggingFaceTextGenerationClient()
