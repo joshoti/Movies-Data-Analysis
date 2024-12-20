96bbb0384b20bbe06579eace05aef6efb6a8024f
joshoti <joshoti539@gmail.com>
2024-12-20 00:10:17 -0800

    Added probe/prediction service to public modules

--- api/predict/__init__.py
+++ api/predict/__init__.py
@@ -1,3 +1,4 @@
 from .predictController import predict_bp
+from .predictService import prediction_service
 
-__all__ = ["predict_bp"]
+__all__ = ["predict_bp", "prediction_service"]
--- api/probe/__init__.py
+++ api/probe/__init__.py
@@ -1,3 +1,4 @@
 from .probeController import probe_bp
+from .probeService import probing_service
 
-__all__ = ["probe_bp"]
+__all__ = ["probe_bp", "probing_service"]
